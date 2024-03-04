82ede3e1d1c5a731da8d033dd2d36913d957a104
artemplohenko@gmail.com <artemplohenko@gmail.com>
2024-03-04 14:20:11 +0200

    added component Formik

--- src/Form.js
+++ src/Form.js
@@ -1,116 +1,51 @@
-import { useFormik } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
-// const validate = (values) => {
-//   const errors = {};
-
-//   if (!values.name) {
-//     errors.name = "Mandatory field!";
-//   } else if (values.name.length < 2) {
-//     errors.name = "Minimum two characters!";
-//   }
-
-//   if (!values.email) {
-//     errors.email = "Mandatory field!";
-//   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
-//     errors.email = "Invalid email address";
-//   }
-
-//   return errors;
-// };
-
-const Form = () => {
-  const formik = useFormik({
-    initialValues: {
-      name: "",
-      email: "",
-      amount: 0,
-      currency: "",
-      text: "",
-      terms: false,
-    },
-    // validate,
-    validationSchema: Yup.object({
-      name: Yup.string().min(2, "Minimum two characters!").required("Required field"),
-      email: Yup.string().email("Mandatory field!").required("Required field"),
-      amount: Yup.number().min(5, "Not less than 5").required("Required field"),
-      currency: Yup.string().required("Select currency"),
-      text: Yup.string().min(10, "Not less than 10"),
-      terms: Yup.boolean().required("Necessary coordination").oneOf([true], "Necessary coordination"),
-    }),
-    onSubmit: (values) => console.log(JSON.stringify(values, null, 2)),
-  });
-
+const CustomForm = () => {
   return (
-    <form className="form" onSubmit={formik.handleSubmit}>
-      <h2>Send a donation</h2>
-      <label htmlFor="name">Your name</label>
-      <input
-        id="name"
-        name="name"
-        type="text"
-        value={formik.values.name}
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-      />
-      {formik.errors.name && formik.touched.name ? <div className="error">{formik.errors.name}</div> : null}
-      <label htmlFor="email">Your mail</label>
-      <input
-        id="email"
-        name="email"
-        type="email"
-        value={formik.values.email}
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-      />
-      {formik.errors.email && formik.touched.email ? <div className="error">{formik.errors.email}</div> : null}
-      <label htmlFor="amount">Quantity</label>
-      <input
-        id="amount"
-        name="amount"
-        type="number"
-        value={formik.values.amount}
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-      />
-      {formik.errors.amount && formik.touched.amount ? <div className="error">{formik.errors.amount}</div> : null}
-      <label htmlFor="currency">Currency</label>
-      <select
-        id="currency"
-        name="currency"
-        value={formik.values.currency}
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-      >
-        <option value="">Choose a currency</option>
-        <option value="USD">USD</option>
-        <option value="UAH">UAH</option>
-        <option value="EUR">EUR</option>
-      </select>
-      {formik.errors.currency && formik.touched.currency ? <div className="error">{formik.errors.currency}</div> : null}
-      <label htmlFor="text">Your message</label>
-      <textarea
-        id="text"
-        name="text"
-        value={formik.values.text}
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-      />
-      {formik.errors.text && formik.touched.text ? <div className="error">{formik.errors.text}</div> : null}
-      <label className="checkbox">
-        <input
-          name="terms"
-          type="checkbox"
-          value={formik.values.terms}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-        />
-        Do you agree with the privacy policy?
-      </label>
-      {formik.errors.terms && formik.touched.terms ? <div className="error">{formik.errors.terms}</div> : null}
-      <button type="submit">Send</button>
-    </form>
+    <Formik
+      initialValues={{ name: "", email: "", amount: 0, currency: "", text: "", terms: false }}
+      validationSchema={Yup.object({
+        name: Yup.string().min(2, "Minimum two characters!").required("Required field"),
+        email: Yup.string().email("Mandatory field!").required("Required field"),
+        amount: Yup.number().min(5, "Not less than 5").required("Required field"),
+        currency: Yup.string().required("Select currency"),
+        text: Yup.string().min(10, "Not less than 10"),
+        terms: Yup.boolean().required("Necessary coordination").oneOf([true], "Necessary coordination"),
+      })}
+      onSubmit={(values) => console.log(JSON.stringify(values, null, 2))}
+    >
+      <Form className="form">
+        <h2>Send a donation</h2>
+        <label htmlFor="name">Your name</label>
+        <Field id="name" name="name" type="text" />
+        <ErrorMessage className="error" name="name" component="div" />
+        <label htmlFor="email">Your mail</label>
+        <Field id="email" name="email" type="email" />
+        <ErrorMessage className="error" name="email" component="div" />
+        <label htmlFor="amount">Quantity</label>
+        <Field id="amount" name="amount" type="number" />
+        <ErrorMessage className="error" name="amount" component="div" />
+        <label htmlFor="currency">Currency</label>
+        <Field id="currency" name="currency" as="select">
+          <option value="">Choose a currency</option>
+          <option value="USD">USD</option>
+          <option value="UAH">UAH</option>
+          <option value="EUR">EUR</option>
+        </Field>
+        <ErrorMessage className="error" name="currency" component="div" />
+        <label htmlFor="text">Your message</label>
+        <Field id="text" name="text" as="textarea" />
+        <ErrorMessage className="error" name="text" component="div" />
+        <label className="checkbox">
+          <Field name="terms" type="checkbox" />
+          Do you agree with the privacy policy?
+        </label>
+        <ErrorMessage className="error" name="terms" component="div" />
+        <button type="submit">Send</button>
+      </Form>
+    </Formik>
   );
 };
 
-export default Form;
+export default CustomForm;
